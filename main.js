685d38669e2f5cd3978d16be01e504c2ebf3c53b
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-05-14 16:52:03 -0700

    Clean up comments

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-let h = window.innerHeight;
-let w = window.innerWidth;
-
 // generate random numbers dependent on inner width and inner height
-let randomHeight = Math.random() * h;
-let randomWidth = Math.random() * w;
+const randomHeight = Math.random() * window.innerHeight;
+const randomWidth = Math.random() * window.innerWidth;
 
-// set background to waldo background
+// Set random waldo background image
 const backgrounds = [
 	'https://wallpaperplay.com/walls/full/9/5/4/92517.jpg',
 	'https://cdn.hipwallpaper.com/i/76/50/iN5lzF.jpg',
@@ -16,43 +13,40 @@ document.body.style.background = `url(${backgrounds[i]})`;
 document.body.style.backgroundPosition = 'center';
 document.body.style.backgroundSize = 'cover';
 
-// create image tag with src attr to (random?) waldo image
+// Waldo image popup
 let waldoImg = document.createElement('img');
 waldoImg.src = 'https://codesmith.io/876620a5b0028d26162b6e7c142be182.jpg';
-waldoImg.style.opacity = 0.75;
-waldoImg.style.borderRadius = '50%';
-
 waldoImg.classList.add('waldo');
 waldoImg.style.top = `${randomHeight}px`;
 waldoImg.style.left = `${randomWidth}px`;
 waldoImg.addEventListener('mouseover', playSound);
+
+// Food image container
 let divTag = document.createElement('div');
 divTag.id = 'foodImg';
 divTag.style.backgroundColor = 'white';
 divTag.style.position = 'fixed';
+divTag.style.textAlign = 'center';
+divTag.style.zIndex = '999';
 divTag.style.top = `${randomHeight + 50}px`;
 divTag.style.left = `${randomWidth}px`;
-divTag.style.textAlign = 'center';
-divTag.style.zIndex = "999"
+
+
+/**
+ * Attach onclick event that fetches from 'theMealDB' API
+ */
 waldoImg.onclick = function () {
 	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
 		.then((res) => res.json())
 		.then((data) => {
-			// let food = document.createElement('img');
-			// food.src = "https://www.themealdb.com/images/media/meals/58oia61564916529.jpg";
-			// food.width = "100"
-			// food.height = "auto";
-			// food.style.border = "thin solid black"
-			// divTag.appendChild(food)
-			console.log(data.meals[0])
 			divTag.innerHTML = `<h3><a href=${data.meals[0].strYoutube}>${data.meals[0].strMeal}</a></h3><br><img width="125" height="auto" src=${data.meals[0].strMealThumb}>`;
 			document.body.append(divTag);
 		});
 };
-waldoImg.onmouseout = function() {
 
-}
-
+/**
+ * Event that listens to mouse over event of the waldo image tag
+ */
 function playSound() {
 	let audio = document.createElement('audio');
 	audio.id = 'ourAudio';
@@ -69,42 +63,6 @@ setTimeout(() => {
 	document.body.append(waldoImg);
 }, 1000);
 
-/**
- * x y
- * 0 1 up
- * 0-1 down
- *-1 0 left
- * 1 0 right
- * -1 1
- * 1 -1
- * -1 -1
- * 1 1
- * random i from 0 to 3 => pair for direction
- */
-// let randX = [0, 0, -1, 1, -1, 1, -1, 1 ]
-// let randY = [1, -1, 0, 0, 1, -1, -1, 1 ]
-
-//  // Grab all tags
-//  let tags = document.getElementsByTagName('*');
-
-//  // Iterate through tags
-//  for (let tag of tags) {
-// 	let i = Math.floor(Math.random() * randX.length);
-// 	setInterval(() => {
-// 		let currTop = tag.offsetTop;
-// 		let currLeft = tag.offsetLeft;
-// 		tag.style.position = "fixed"
-
-// 		tag.style.top = `${currTop + randY[i]}px`
-// 		tag.style.left = `${currLeft + randX[i]}px`
-// 	},1)
-// }
-
-// store the current offset left and offset top
-
-// style the top with new direction
-// style the left with new direction
-
 /*
 // timer or onclick or keypress
 // popup html description of extension
@@ -118,17 +76,6 @@ setTimeout(() => {
 
 
 // presentation: explain idea, demo, tech challenges, stretch features
-
-https://p7.hiclipart.com/preview/356/362/533/where-s-wally-desktop-wallpaper-shoe-clip-art-computer-thumbnail.jpg
-
-https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_740,w_1100/v1555285691/shape/mentalfloss/waldomain.png?itok=SpMGe5nM
-
-https://img.pngio.com/finger-graphics-illustration-transparent-png-image-clipart-wheres-waldo-png-900_900.png
-
-
-
-https://i0.wp.com/www.thelocalvoice.net/oxford/wp-content/uploads/2012/06/fuknwaldos.gif
-
 
 dave
 https://codesmith.io/876620a5b0028d26162b6e7c142be182.jpg
